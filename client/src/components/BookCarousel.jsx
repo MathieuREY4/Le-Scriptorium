a772801bd37bf831edee8f5a92af9ef7bdd862d7
Mathieu REY <mathieu.rey33@gmail.com>
2024-09-30 02:25:02 +0200

    :sparkles: add images in bookCarousel

--- client/src/components/BookCarousel.jsx
+++ client/src/components/BookCarousel.jsx
@@ -1,15 +1,22 @@
 import "../styles/BookCarousel.css";
+import Chimère from "../assets/images/chimère.jpg";
 
 export default function BookCarousel() {
+  const books = ["book-1", "book-2", "book-3", "book-4", "book-5"];
+
   return (
     <section className="book-carousel">
       <div className="carousel-arrow left-arrow">&lt;</div>
       <div className="carousel-container">
-        <div className="book-card">Book 1</div>
-        <div className="book-card">Book 2</div>
-        <div className="book-card">Book 3</div>
-        <div className="book-card">Book 4</div>
-        <div className="book-card">Book 5</div>
+        {books.map((book) => (
+          <div className="book-card" key={book}>
+            <img
+              src={Chimère}
+              alt="Couverture du livre Chimère"
+              className="book-image"
+            />
+          </div>
+        ))}
       </div>
       <div className="carousel-arrow right-arrow">&gt;</div>
     </section>
