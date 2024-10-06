47758b4a182f8c8f67c3dfdb0bd680999c088180
Mathieu <Mathieu.rey33@gmail.com>
2024-10-06 02:15:11 +0200

    :hammer: add auth, signup, login and modif navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,60 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/authContext";
 import Logo from "../assets/images/Logo.png";
-
 import "../styles/Navbar.css";
 
 export default function Navbar() {
+  const navigate = useNavigate();
+  const authContext = useAuth();
+
+  const handleLogin = () => {
+    navigate("/login");
+  };
+
+  const handleLogout = async () => {
+    if (authContext?.logout) {
+      await authContext.logout();
+      navigate("/");
+    }
+  };
+
   return (
     <nav className="navbar">
       <div className="nav-left">
-        <a href="#home">Accueil</a>
-        <a href="#publish">Publier</a>
+        <Link to="/">Accueil</Link>
+        {authContext?.user && <Link to="/publish">Publier</Link>}
       </div>
       <div className="nav-logo">
-        <img src={Logo} alt="Logo" />
+        <Link to="/">
+          <img src={Logo} alt="Logo" />
+        </Link>
       </div>
       <div className="nav-right">
-        <a href="#signup">Inscription</a>
+        {authContext?.user ? (
+          <>
+            <button
+              type="button"
+              onClick={handleLogout}
+              className="logout-button"
+            >
+              Déconnexion
+            </button>
+            <Link to="/user">
+              <img
+                src={`https://api.dicebear.com/6.x/initials/svg?seed=${authContext.user.fullname}`}
+                alt={authContext.user.fullname}
+                className="user-avatar"
+              />
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link to="/register">Inscription</Link>
+            <button type="button" onClick={handleLogin}>
+              Connexion
+            </button>
+          </>
+        )}
       </div>
     </nav>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,10 +5,21 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
 
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <SignUp />,
   },
 ]);
 
